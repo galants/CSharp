17ae0947b1d7de5dd4e2930ac23c7f61097b8309
Vencislav Nikolov <vencislav.nikolov@hotmail.com>
2014-10-01 22:14:29 +0300

    Exam

---
+++ Exam/C# Basics Exam 26 August 2014/CSharp Basics Exam 26 August 2014/01.Tic-Tac-ToePower/TicTacToePower.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class TicTacToePower
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int v = int.Parse(Console.ReadLine());
+
+        int index = y * 3 + x + 1;
+
+        int value = v + index - 1;
+
+        decimal result = 1;
+        for (int i = 0; i < index; i++)
+        {
+            result *= value;
+        }
+
+        Console.WriteLine(result);
+
+    }
+}
